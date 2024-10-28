5bdec919b774f486231d0ffa037d5f624908f7db
a24braisgp <a24braisgp@iessanclemente.net>
2024-10-28 20:02:22 +0100

    falta exercicio 6

--- exercicios1.js
+++ exercicios1.js
@@ -1135,5 +1135,65 @@ console.log(
 	})
 );
 
-// console.log('f)');
-// console.log(inventors.reduce((acc, curr) => ));
+console.log('f)');
+const sumaAños = inventors.map(({ year, passed }) => {
+	return { year, passed };
+});
+console.log(sumaAños);
+
+const anos = [];
+sumaAños.forEach(({ year, passed }) => anos.push(passed - year));
+
+console.log(
+	`O total de anos vividos polos científicos é : ${anos.reduce(
+		(acc, curr) => acc + curr
+	)}`
+);
+
+console.log('g)');
+inventors.sort((a, b) => {
+	const livedA = a.passed - a.year;
+	const livedB = b.passed - b.year;
+	if (livedA < livedB) {
+		return 1;
+	}
+	if (livedA > livedB) {
+		return -1;
+	}
+	// names must be equal
+	return 0;
+});
+console.log(inventors);
+
+console.log('--------------------------');
+
+// 6. Dada a seguinte información, obtén un obxecto con unha propiedade para cada
+// medio de transporte, indicando o número de veces que se repite no array. É dicir, o resultado debería ser {car: 5, truck: 3, bike: 2, walk: 2, van: 2, pogostick: 1}. Intentar facer o exercicio usando o método reduce
+const data = [
+	'car',
+	'car',
+	'truck',
+	'truck',
+	'bike',
+	'walk',
+	'car',
+	'van',
+	'bike',
+	'walk',
+	'car',
+	'van',
+	'car',
+	'truck',
+	'pogostick',
+];
+
+const dataObject = {};
+dataObject = data.reduce((acc, curr) => {
+	if (Object.hasOwn(acc)) {
+		[acc] = [acc] + 1;
+	} else if (!Object.hasOwn(curr)) {
+		[curr] = 1;
+	}
+}, {});
+
+console.log(dataObject);
