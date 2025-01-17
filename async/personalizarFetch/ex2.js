650aad248280f3b71ad951463105ba94c8043338
a24braisgp <a24braisgp@iessanclemente.net>
2025-01-17 18:28:22 +0100

    a

--- async/personalizarFetch/ex2.js
+++ async/personalizarFetch/ex2.js
@@ -3,6 +3,7 @@ let botonCrear = document.getElementById('botonCrear');
 let divLista = document.getElementById('divLista');
 let input = document.getElementById('toDoName');
 const reqUrl = 'https://jsonplaceholder.typicode.com/todos?_limit=10';
+let id = 201;
 
 botonCrear.addEventListener('click', crearToDo);
 actualizarListaToDo();
@@ -12,7 +13,7 @@ function crearToDo(event) {
 	let newTodoTitle = input.value;
 	console.log(newTodoTitle);
 
-	crearBoton(newTodoTitle);
+	crearBotonDeInput(newTodoTitle);
 	crearToDoDB(newTodoTitle);
 }
 
@@ -24,15 +25,17 @@ function crearToDoDB(toDoTitle) {
 			body: JSON.stringify({
 				userId: 1,
 				title: `${toDoTitle}`,
+				completed: false,
 			}),
 			headers: {
-				'Content-type': 'application/json: charset-UTF8',
+				'Content-type': 'application/json; charset=UTF-8',
 			},
 		}
 	);
 	fetch(reqCrearToDo)
 		.then((response) => response.json())
-		.then((dataResponse) => console.log(dataResponse));
+		.then((dataResponse) => console.log(dataResponse))
+		.catch((error) => console.log(error));
 }
 
 function actualizarListaToDo() {
@@ -44,19 +47,59 @@ function actualizarListaToDo() {
 
 function mostrarLista(data) {
 	for (const todo of data) {
-		crearBoton(todo.title);
+		crearBoton(todo);
 	}
 }
 
-function crearBoton(toDoTitle) {
+function crearBoton(toDo) {
 	let newEntry = document.createElement('button');
 	newEntry.classList.add('btn');
 	newEntry.classList.add('btn-outline-info');
 	newEntry.addEventListener('click', completarToDo);
+	newEntry.addEventListener('contextmenu', disableCompletar);
 	newEntry.style.margin = '5px';
-	newEntry.innerText = toDoTitle;
+	newEntry.dataset.toDoId = toDo.id;
+	newEntry.innerText = toDo.title;
 	divLista.append(newEntry);
 }
 
-//TODO cambiar background y borrar de la lista
-function completarToDo(event) {}
+function crearBotonDeInput(toDo) {
+	let newEntry = document.createElement('button');
+	newEntry.classList.add('btn');
+	newEntry.classList.add('btn-outline-info');
+	newEntry.addEventListener('click', completarToDo);
+	newEntry.addEventListener('contextmenu', disableCompletar);
+	newEntry.style.margin = '5px';
+	newEntry.dataset.toDoId = id;
+	newEntry.innerText = toDo;
+	divLista.append(newEntry);
+	id++;
+}
+
+function disableCompletar(event) {
+	event.target.classList.remove('completed');
+}
+
+function completarToDo(event) {
+	if (event.target.classList.contains('completed')) {
+		deleteToDo(event.target);
+	} else {
+		event.target.classList.add('completed');
+	}
+}
+
+function deleteToDo(toDoToDelete) {
+	console.log(toDoToDelete);
+
+	const toDoId = toDoToDelete.dataset.toDoId;
+	const reqDelete = new Request(
+		`https://jsonplaceholder.typicode.com/todos/${toDoId}`,
+		{
+			method: 'DELETE',
+		}
+	);
+	fetch(reqDelete);
+	divLista.removeChild(toDoToDelete);
+}
+
+//TODO implementar actualización
