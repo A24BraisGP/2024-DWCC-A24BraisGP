cc9a95c7a47e47fda86f13b045f7ab071aec8dd3
A24BraisGP <a24braisgp@iessanclemente.net>
2025-02-18 09:10:36 +0100

    engado función para os logs

--- vue/renderizadoListas/ex4Xogo/index.js
+++ vue/renderizadoListas/ex4Xogo/index.js
@@ -47,40 +47,40 @@ createApp({
 		monsterAttack() {
 			let monsterAttack = Math.floor(Math.random() * (15 - 8) + 8);
 			this.player.life -= monsterAttack;
-			this.logs.push({
-				user: 'Monstro',
-				logClass: 'log--monster',
-				pointClass: 'log--damage',
-				action: 'ataca con',
-				points: monsterAttack,
-			});
+			this.addLog(
+				'Monstro',
+				'log--monster',
+				'log--damage',
+				'ataca con',
+				monsterAttack
+			);
 		},
 		normalAttack() {
 			let playerAttack = Math.floor(Math.random() * (12 - 5) + 5);
-			let monsterDamage = this.monsterAttack();
 			this.monster.life -= playerAttack;
-			let logPlayer = `Vostede ataca con ${playerAttack}`;
-			this.logs.push({
-				user: 'Vostede',
-				logClass: 'log--player',
-				pointClass: 'log--damage',
-				action: 'ataca con',
-				points: playerAttack,
-			});
+			this.monsterAttack();
+			this.addLog(
+				'Vostede',
+				'log--player',
+				'log--damage',
+				'ataca con',
+				playerAttack
+			);
 		},
 		specialAttack() {
 			let playerAttack = Math.floor(Math.random() * (25 - 10) + 10);
 			this.monster.life -= playerAttack;
-			let monsterDamage = this.monsterAttack();
+			this.monsterAttack();
+
 			this.turnCount = 0;
 			this.specialActive = false;
-			this.logs.push({
-				user: 'Vostede',
-				logClass: 'log--player',
-				pointClass: 'log--damage',
-				action: 'ataca con',
-				points: playerAttack,
-			});
+			this.addLog(
+				'Vostede',
+				'log--player',
+				'log--damage',
+				'ataca con',
+				playerAttack
+			);
 		},
 		healing() {
 			let oldLife = this.player.life;
@@ -90,13 +90,23 @@ createApp({
 			} else {
 				this.player.life += healingPoints;
 			}
-			let monsterDamage = this.monsterAttack();
+			this.monsterAttack();
+
+			this.addLog(
+				'Vostede',
+				'log--player',
+				'log--heal',
+				'recupera',
+				healingPoints
+			);
+		},
+		addLog(user, logClass, pointClass, action, points) {
 			this.logs.push({
-				user: 'Vostede',
-				logClass: 'log--player',
-				pointClass: 'log--heal',
-				action: 'recupera',
-				points: healingPoints,
+				user: user,
+				logClass: logClass,
+				pointClass: pointClass,
+				action: action,
+				points: points,
 			});
 		},
 		checkStatus() {
@@ -105,13 +115,13 @@ createApp({
 				this.player.life <= this.MINLIFE
 			) {
 				this.victor = 'monster';
-				this.player.life = this.MINLIFE + 1;
+				this.player.life = this.MINLIFE;
 				this.gameEnded = true;
 				return;
 			}
 			if (this.monster.life <= this.MINLIFE) {
 				this.victor = 'player';
-				this.monster.life = this.MINLIFE + 1;
+				this.monster.life = this.MINLIFE;
 
 				this.gameEnded = true;
 
@@ -131,7 +141,7 @@ createApp({
 		playerGiveUp() {
 			this.gameEnded = true;
 			this.victor = 'monster';
-			this.player.life = this.MINLIFE + 1;
+			this.player.life = this.MINLIFE;
 		},
 	},
 	watch: {
