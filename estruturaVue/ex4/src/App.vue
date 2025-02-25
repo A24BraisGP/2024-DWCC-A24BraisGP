1bb81a0cc3141db9d73cfa300b40b599ccfab8a0
A24BraisGP <a24braisgp@iessanclemente.net>
2025-02-25 12:56:48 +0100

    Refactor(ex4):Finished ex4

--- estruturaVue/ex4/src/App.vue
+++ estruturaVue/ex4/src/App.vue
@@ -1,17 +1,38 @@
 <script>
 import Info from '@/components/Info.vue';
+import Form from '@/components/Form.vue';
 export default {
 	data() {
 		return {
-			nome: '',
-			anoNacemento: 0,
+			nome: 'Brais García',
+			anoNacemento: 1999,
 		};
+	},
+	components: {
+		Info,
+		Form,
+	},
+	methods: {
+		actualizarNome(newName) {
+			this.nome = newName;
+		},
+		actualizarAno(newAno) {
+			this.anoNacemento = newAno;
+		},
 	},
 };
 </script>
 
 <template>
-	<Info :nome="nome" :anoNacemento="anoNacemento"></Info>
+	<Form
+		:nome="nome"
+		:anoNacemento="anoNacemento"
+		@cambiar-ano="actualizarAno"
+		@cambiar-nome="actualizarNome"
+	></Form>
+	<div>
+		<Info :nome="nome" :anoNacemento="anoNacemento"></Info>
+	</div>
 </template>
 
 <style></style>
---
+++ estruturaVue/ex4/src/components/Form.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+	data() {
+		return {};
+	},
+	props: {
+		nome: {
+			type: String,
+			required: true,
+		},
+		anoNacemento: { type: Number, required: true },
+	},
+	emits: ['cambiarNome', 'cambiarAno'],
+};
+</script>
+
+<template>
+	<div>
+		<form action="" method="get">
+			Name:
+			<input
+				type="text"
+				:value="nome"
+				@input="$emit('cambiarNome', $event.target.value)"
+			/>
+			Ano de Nacemento:
+			<input
+				type="number"
+				:value="anoNacemento"
+				@input="$emit('cambiarAno', $event.target.value)"
+			/>
+		</form>
+	</div>
+</template>
+
+<style></style>
