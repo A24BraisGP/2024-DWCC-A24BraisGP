457e1ab0aed51d1988b2e42e31d9fa4f1f620363
A24BraisGP <a24braisgp@iessanclemente.net>
2025-03-10 17:39:47 +0100

    started v-model custom

---
+++ estruturaVue/customVmodel/ex/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import CustomInput from './components/CustomInput.vue';
+
+export default {
+	data() {
+		return {
+			opcionPulsada: '',
+		};
+	},
+	components: {
+		CustomInput,
+	},
+};
+</script>
+
+<template>
+	<form action="">
+		<CustomInput v-model:opcion-pulsada="opcionPulsada"></CustomInput>
+		<div class="grid place-items-center">
+			<button type="button" class="btn btn-primary">Enviar</button>
+		</div>
+	</form>
+
+	<div class="grid place-items-center">
+		<p class="text-white text-2xl">Vostede pulsou: {{ opcionPulsada }}</p>
+	</div>
+</template>
+<style></style>
---
+++ estruturaVue/customVmodel/ex/src/components/CustomInput.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+	props: ['opcionPulsada'],
+	emits: ['update:opcionPulsada'],
+	data() {
+		return {};
+	},
+};
+</script>
+
+<template>
+	<ul class="grid grid-cols-3 place-items-center my-4">
+		<li>
+			<button
+				type="button"
+				class="btn btn-accent focus:outline-2 focus:outline-offset-2 focus:outline-green-200"
+				@click.stop="
+					$emit('update:opcionPulsada', $event.target.innerText)
+				"
+			>
+				Baixo
+			</button>
+		</li>
+		<li>
+			<button
+				class="btn btn-accent focus:outline-2 focus:outline-offset-2 focus:outline-green-200"
+				type="button"
+				@click.stop="
+					$emit('update:opcionPulsada', $event.target.innerText)
+				"
+			>
+				Medio
+			</button>
+		</li>
+		<li>
+			<button
+				class="btn btn-accent focus:outline-2 focus:outline-offset-2 focus:outline-green-200"
+				type="button"
+				@click.stop="
+					$emit('update:opcionPulsada', $event.target.innerText)
+				"
+			>
+				Alto
+			</button>
+		</li>
+	</ul>
+</template>
